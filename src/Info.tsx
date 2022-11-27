9f099a423443078572a6e8833702b17addf1cc6b
pavel11988 <kossoua88@gmail.com>
2022-11-27 20:01:10 +0200

    fix

--- src/Info.tsx
+++ src/Info.tsx
@@ -19,8 +19,15 @@ const Info: React.FC = () => {
 	const entranceOffset = interpolate(entrance, [0, 1], [height, 0]);
 
 	return (
-		<div style={{transform: `translateY(${entranceOffset}px)`}}>
-			<Img src={require('./public/assets/test_container.png')} />
+		<div
+			style={{transform: `translateY(${entranceOffset}px)`}}
+			className="absolute bottom-0 bg-orange-600 w-[669px] h-[223px] px-10"
+		>
+			<p className=" text-white text-4xl pt-10 mb-6">
+				South pacific mechanics are looking for{' '}
+				<span className="text-black">Diesel Mechanics</span> across Aotearoa
+			</p>
+			<p className="text-white text-2xl ">tradejobs.nz</p>
 		</div>
 	);
 };
